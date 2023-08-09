c6caa8cfb29c1a2b2e16828a8064a5230705f272
leandreane <monk-86@inbox.ru>
2023-08-09 15:47:53 +0300

    first commit

---
+++ src/test/java/ru/netology/rest/CheckoutPostmanEcho.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CheckoutPostmanEcho {
+    @Test
+    void shouldCheckoutPostmanEcho() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
